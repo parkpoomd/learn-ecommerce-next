42b851e717f6f0766e87d832f2114502766a3da5
parkpoomd <parkpoom.d@gmail.com>
2022-06-02 16:51:48 +0700

    Reusable layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import type { AppProps } from 'next/app';
-import { Layout } from '@components/common';
+import { FC } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps & { Component: { Layout: FC } }) {
+  const Layout = Component.Layout;
+
   return (
-    <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { InferGetStaticPropsType } from 'next';
 import getAllProducts from '@framework/product/get-all-products';
 import { getConfig } from '@framework/api/config';
+import { Layout } from '@components/common';
 
 export async function getStaticProps() {
   const config = getConfig();
@@ -17,5 +18,7 @@ export async function getStaticProps() {
 export default function Home({
   products,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return <div>{products[0].name}</div>;
+  return <div>{JSON.stringify(products)}</div>;
 }
+
+Home.Layout = Layout;
