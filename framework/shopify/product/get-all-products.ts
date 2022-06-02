c447aa5906d36f9f5a1b824080139ea1a953bf2e
parkpoomd <parkpoom.d@gmail.com>
2022-06-02 12:49:26 +0700

    projects config file

---
+++ framework/shopify/product/get-all-products.ts
@@ -0,0 +1,21 @@
+import { fetchApi, normalizeProduct, getAllProductsQuery } from '../utils';
+import { ProductConnection } from '../schema';
+import { Product } from '@common/types/product';
+
+type ReturnType = {
+  products: ProductConnection;
+};
+
+const getAllProducts = async (): Promise<Product[]> => {
+  const { data } = await fetchApi<ReturnType>({
+    query: getAllProductsQuery,
+  });
+
+  const products =
+    data.products.edges.map(({ node: product }) => normalizeProduct(product)) ??
+    [];
+
+  return products;
+};
+
+export default getAllProducts;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { withFrameworkConfig } = require('./framework/common/config');
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = withFrameworkConfig({
+  reactStrictMode: true,
+  framework: {
+    name: 'shopify',
+  },
+  i18n: {
+    locales: ['en-US', 'th'],
+    defaultLocale: 'en-US',
+  },
+});
+
+module.exports = nextConfig;
