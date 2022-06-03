9355f713da09647a3d3dafaeb399e8a3a952b892
parkpoomd <parkpoom.d@gmail.com>
2022-06-03 12:35:48 +0700

    Product Tag

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -2,6 +2,7 @@ import { Product } from '@common/types/product';
 import { FC } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import style from './ProductCard.module.css';
 
 interface Props {
   product: Product;
@@ -12,12 +13,12 @@ const placeholderImage = '/product-image-placeholder.svg';
 const ProductCard: FC<Props> = ({ product }) => {
   return (
     <Link href={`/products/${product.slug}`}>
-      <a>
-        <div>
-          <h3>
+      <a className={style.root}>
+        <div className={style.productTag}>
+          <h3 className={style.productTitle}>
             <span>{product.name}</span>
           </h3>
-          <span>14 $</span>
+          <span className={style.productPrice}>14 $</span>
         </div>
         {product.images && (
           <Image
