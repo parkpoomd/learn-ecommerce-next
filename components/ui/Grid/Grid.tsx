20f1137199a99d709085cd689c72d86cefc53f3b
parkpoomd <parkpoom.d@gmail.com>
2022-06-03 09:22:42 +0700

    Grid stylings

--- components/ui/Grid/Grid.tsx
+++ components/ui/Grid/Grid.tsx
@@ -1,7 +1,8 @@
 import { FC, ReactNode } from 'react';
+import style from './Grid.module.css';
 
 const Grid: FC<ReactNode> = ({ children }) => {
-  return <div className="root-grid">{children}</div>;
+  return <div className={style.root}>{children}</div>;
 };
 
 export default Grid;
