0b44998a84391ffd85c80279b8adabd38a00468b
parkpoomd <parkpoom.d@gmail.com>
2022-06-03 23:23:54 +0700

    Product Variant

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -6,33 +6,40 @@ import style from './ProductCard.module.css';
 
 interface Props {
   product: Product;
+  variant?: 'simple' | 'slim';
 }
 
 const placeholderImage = '/product-image-placeholder.svg';
 
-const ProductCard: FC<Props> = ({ product }) => {
+const ProductCard: FC<Props> = ({ product, variant = 'simple' }) => {
   return (
     <Link href={`/products/${product.slug}`}>
       <a className={style.root}>
-        <div className={style.productBg}></div>
-        <div className={style.productTag}>
-          <h3 className={style.productTitle}>
-            <span>{product.name}</span>
-          </h3>
-          <span className={style.productPrice}>
-            {product.price.value} {product.price.currencyCode}
-          </span>
-        </div>
-        {product.images && (
-          <Image
-            className={style.productImage}
-            alt={product.name ?? 'Product image'}
-            src={product.images[0].url ?? placeholderImage}
-            height={540}
-            width={540}
-            quality="85"
-            layout="responsive"
-          />
+        {variant === 'slim' ? (
+          <>SLIM PRODUCT</>
+        ) : (
+          <>
+            <div className={style.productBg}></div>
+            <div className={style.productTag}>
+              <h3 className={style.productTitle}>
+                <span>{product.name}</span>
+              </h3>
+              <span className={style.productPrice}>
+                {product.price.value} {product.price.currencyCode}
+              </span>
+            </div>
+            {product.images && (
+              <Image
+                className={style.productImage}
+                alt={product.name ?? 'Product image'}
+                src={product.images[0].url ?? placeholderImage}
+                height={540}
+                width={540}
+                quality="85"
+                layout="responsive"
+              />
+            )}
+          </>
         )}
       </a>
     </Link>
