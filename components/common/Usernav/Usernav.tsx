cb4f0d7a92a1ede26c560017cc0dfccf09f0bc51
parkpoomd <parkpoom.d@gmail.com>
2022-06-04 11:51:39 +0700

    Icons

--- components/common/Usernav/Usernav.tsx
+++ components/common/Usernav/Usernav.tsx
@@ -1,15 +1,20 @@
 import { FC } from 'react';
 import Link from 'next/link';
+import { Bag as Cart, Heart } from '@components/icons';
 import style from './Usernav.module.css';
 
 const Usernav: FC = () => {
   return (
     <nav>
       <ul className={style.list}>
-        <li className={style.item}>Cart</li>
+        <li className={style.item}>
+          <Cart />
+        </li>
         <li className={style.item}>
           <Link href="/">
-            <a>Wishlist</a>
+            <a>
+              <Heart />
+            </a>
           </Link>
         </li>
       </ul>
