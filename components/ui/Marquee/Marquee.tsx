7e0ab87a904a257b269f414e47a3d702c6428701
parkpoomd <parkpoom.d@gmail.com>
2022-06-04 10:12:06 +0700

    Marquee variant

--- components/ui/Marquee/Marquee.tsx
+++ components/ui/Marquee/Marquee.tsx
@@ -1,13 +1,18 @@
 import { FC, ReactNode } from 'react';
 import style from './Marquee.module.css';
+import cn from 'classnames';
 
 interface Props {
   children: ReactNode[];
+  variant?: 'primary' | 'secondary';
 }
 
-const Marquee: FC<Props> = ({ children }) => {
+const Marquee: FC<Props> = ({ children, variant = 'primary' }) => {
+  const rootClassName = cn(style.root, {
+    [style.secondary]: variant === 'secondary',
+  });
   return (
-    <div className={style.root}>
+    <div className={rootClassName}>
       <div className={style.container}>{children}</div>
     </div>
   );
