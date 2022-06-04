600f4676fbcb97740c6b7c22b869beddffd79d3f
parkpoomd <parkpoom.d@gmail.com>
2022-06-04 10:22:50 +0700

    Footer

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 import style from './Layout.module.css';
+import { Footer } from '@components/common';
 
 const Layout: FC = ({ children }) => {
   return (
     <div className={style.root}>
       <main className="fit">{children}</main>
+      <Footer />
     </div>
   );
 };
