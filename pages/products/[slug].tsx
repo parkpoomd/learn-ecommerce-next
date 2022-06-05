aae4f9fa6111606c59072c901ab8d14d38a8b1db
parkpoomd <parkpoom.d@gmail.com>
2022-06-05 11:47:09 +0700

    Pass variables to query

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -10,6 +10,7 @@ import {
 // fetch all of the products slugs
 export const getStaticPaths: GetStaticPaths = async () => {
   const config = getConfig();
+
   const { products } = await getAllProductsPaths(config);
 
   return {
@@ -23,7 +24,11 @@ export const getStaticProps = async ({
   params,
 }: GetStaticPropsContext<{ slug: string }>) => {
   const config = getConfig();
-  const { product } = await getProduct(config);
+
+  const { product } = await getProduct({
+    config,
+    variables: { slug: params?.slug },
+  });
 
   return {
     props: {
