18995a271338628f9ed359a11ce2786421f03d9b
parkpoomd <parkpoom.d@gmail.com>
2022-06-06 12:57:59 +0700

    Active Choice

--- components/product/Swatch/Swatch.tsx
+++ components/product/Swatch/Swatch.tsx
@@ -1,26 +1,37 @@
 import { FC } from 'react';
 import style from './Swatch.module.css';
+import cn from 'classnames';
 import { Check } from '@components/icons';
 interface Props {
   color?: string;
   label?: string;
+  active?: boolean;
   variant?: 'size' | 'color' | string;
   onClick: () => void;
 }
 
-const Swatch: FC<Props> = ({ color, label, variant, ...rest }) => {
+const Swatch: FC<Props> = ({ color, label, variant, active, ...rest }) => {
   label = label?.toLowerCase();
   variant = variant?.toLowerCase();
+
+  const rootClassName = cn(style.root, {
+    [style.active]: active,
+    [style.color]: color,
+    [style.size]: variant === 'size',
+  });
 
   return (
     <button
       style={color ? { backgroundColor: color } : {}}
-      className={style.root}
+      className={rootClassName}
       {...rest}
     >
-      {/* <span>
-        <Check />
-      </span> */}
+      {variant === 'color' && active && (
+        <span>
+          <Check />
+        </span>
+      )}
+
       {variant === 'size' ? label : null}
     </button>
   );
