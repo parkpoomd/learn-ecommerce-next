34391fe60a8b9ef5a7357c9ab5cfbbee87c1496d
parkpoomd <parkpoom.d@gmail.com>
2022-06-07 22:08:49 +0700

    Use Api provider

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -4,19 +4,22 @@ import { Footer, Navbar } from '@components/common';
 import { Sidebar } from '@components/ui';
 import { CartSidebar } from '@components/cart';
 import { useUI } from '@components/ui/context';
+import { ApiProvider } from '@common';
 
 const Layout: FC = ({ children }) => {
   const { isSidebarOpen, closeSidebar } = useUI();
 
   return (
-    <div className={style.root}>
-      <Navbar />
-      <Sidebar onClose={closeSidebar} isOpen={isSidebarOpen}>
-        <CartSidebar />
-      </Sidebar>
-      <main className="fit">{children}</main>
-      <Footer />
-    </div>
+    <ApiProvider>
+      <div className={style.root}>
+        <Navbar />
+        <Sidebar onClose={closeSidebar} isOpen={isSidebarOpen}>
+          <CartSidebar />
+        </Sidebar>
+        <main className="fit">{children}</main>
+        <Footer />
+      </div>
+    </ApiProvider>
   );
 };
 
--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -8,6 +8,7 @@ import { ProductSlider, Swatch } from '@components/product';
 import { Choices, getVariant } from '../helpers';
 import { useUI } from '@components/ui/context';
 import useAddItem from '@framework/cart/use-add-item';
+import { useApiProvider } from '@common';
 
 interface Props {
   product: Product;
@@ -15,6 +16,7 @@ interface Props {
 
 const ProductView: FC<Props> = ({ product }) => {
   const [choices, setChoices] = useState<Choices>({});
+  const api = useApiProvider();
   const { openSidebar } = useUI();
   const addItem = useAddItem();
 
