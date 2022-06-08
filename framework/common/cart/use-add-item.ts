ef746de894447bb1b5bc5ff67ad16e13051533d7
parkpoomd <parkpoom.d@gmail.com>
2022-06-08 17:04:38 +0700

    Provide fetch

--- framework/common/cart/use-add-item.ts
+++ framework/common/cart/use-add-item.ts
@@ -1,9 +1,13 @@
 import { useHook } from '@common/utils/use-hook';
 
 const useAddItem = () => {
-  const hook = useHook((hooks) => hooks.cart.useAddItem);
+  const hook = useHook((hooks) => {
+    return hooks.cart.useAddItem;
+  });
 
-  return hook.useHook();
+  return hook.useHook({
+    fetch: hook.fetcher,
+  });
 };
 
 export default useAddItem;
