95fb36dc41b193f10201a323ffec668bcf5dfc46
parkpoomd <parkpoom.d@gmail.com>
2022-06-08 16:04:44 +0700

    Context types

--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -16,7 +16,8 @@ interface Props {
 
 const ProductView: FC<Props> = ({ product }) => {
   const [choices, setChoices] = useState<Choices>({});
-  const api = useApiProvider();
+  const { hooks, fetcher } = useApiProvider();
+
   const { openSidebar } = useUI();
   const addItem = useAddItem();
 
